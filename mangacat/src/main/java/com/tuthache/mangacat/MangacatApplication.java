09018cd013db4652e2ab234576c922d3f8b69feb
Tuthache <64441477+Tuthache@users.noreply.github.com>
2023-10-04 00:42:18 -0700

    Spring Boot Framework project files added

---
+++ mangacat/src/main/java/com/tuthache/mangacat/MangacatApplication.java
@@ -0,0 +1,13 @@
+package com.tuthache.mangacat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangacatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangacatApplication.class, args);
+	}
+
+}
