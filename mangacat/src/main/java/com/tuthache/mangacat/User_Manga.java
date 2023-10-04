ce4e4c1b0c397351640ef90bdcd73f8cf5b83234
Tuthache <64441477+Tuthache@users.noreply.github.com>
2023-10-04 01:40:45 -0700

    Added in JPA dependency to making communication with database easier

--- mangacat/src/main/java/com/tuthache/mangacat/User_Manga.java
+++ mangacat/src/main/java/com/tuthache/mangacat/User_Manga.java
@@ -3,9 +3,8 @@ package com.tuthache.mangacat;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EntityScan
-@Table(name = "Users")
 public class User_Manga {
-    
+
     private Long id;
     private User user;
     private Manga manga;
