b82f78f03a2a0fbaeea487b8eb96e2e7da201eee
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-06-23 03:03:15 -0700

    Routing for About, Signup, Login, and Home pages created

---
+++ client/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="py-24 bg-blue-600 text-white text-center">
+        <h1 className="text-4xl font-bold mb-4">About Our Expense Tracker</h1>
+        <p className="text-lg">
+          Simplify your finances, track your expenses, and achieve your
+          financial goals with our intuitive expense tracker app.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
