7789e7f706207201282e50543ab64f5327e3b18d
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-02 22:18:29 -0700

    Made navigation bar for about page

--- client/src/components/AboutNav.js
+++ client/src/components/AboutNav.js
@@ -1,6 +1,22 @@
 import React from "react";
 
 const AboutNav = () => {
-  return <></>;
+  return (
+    <nav className="flex justify-between items-center bg-pink-300 text-black p-4 py-10">
+      <div className="text-xl font-bold">
+        <a href="/">MangaCat</a>
+      </div>
+      <div>
+        <a href="/login" className="mx-8 text-xl hover:text-red-400">
+          Login
+        </a>
+      </div>
+      <div>
+        <a href="/signup" className="mx-8 text-xl hover:text-red-400">
+          Sign Up
+        </a>
+      </div>
+    </nav>
+  );
 };
 export default AboutNav;
--- client/src/components/DefaultNav.js
+++ client/src/components/DefaultNav.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 const DefaultNav = () => {
-  return <></>;
+  return (
+    <nav className="flex justify-between items-center bg-pink-300 text-black p-4 py-10"></nav>
+  );
 };
 export default DefaultNav;
