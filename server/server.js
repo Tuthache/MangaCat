c3880d6aef0e68431f9989c8899fef15cf46fa2e
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-09 22:21:32 -0700

    Backend server set up

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const connection = require("./config/connection.js");
+const express = require("express");
+
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+
+const cors = require("cors");
+const userRoutes = require("./routes/user");
+const mangaRoutes = require("./routes/manga");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const sessionStorageOptions = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "Expense_Tracker",
+  createDatabaseTable: true,
+  schema: {
+    tableName: "sessions",
+    columnNames: {
+      session_id: "session_id",
+      expires: "expires",
+      data: "data",
+    },
+  },
+};
+const sessionStorage = new MySQLStore(sessionStorageOptions, connection);
+app.use(session({}));
