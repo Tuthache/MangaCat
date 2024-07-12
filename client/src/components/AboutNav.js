774d8a3e699af28b559ffa8457db9c90cf1c1b7c
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-11 20:33:20 -0700

    Finished About page layout

--- client/src/components/AboutNav.js
+++ client/src/components/AboutNav.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const AboutNav = () => {
   return (
-    <nav className="flex justify-between items-center bg-pink-300 text-black p-4 py-10">
+    <nav className="flex justify-between items-center bg-red-300 text-white p-4 py-10">
       <div className="text-xl font-bold">
         <a href="/">MangaCat</a>
       </div>
       <div>
-        <a href="/login" className="mx-8 text-xl hover:text-red-400">
+        <a href="/login" className="mx-8 text-xl hover:text-gray-400">
           Login
         </a>
-        <a href="/signup" className="mx-8 text-xl hover:text-red-400">
+        <a href="/signup" className="mx-8 text-xl hover:text-gray-400">
           Sign Up
         </a>
       </div>
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,14 +5,52 @@ const About = () => {
   return (
     <>
       <AboutNav />
-      <div className="bg-gray-100">
-        <div className="py-24 bg-blue-600 text-white text-center">
-          <h1 className="text-4xl font-bold mb-4">About Our Expense Tracker</h1>
+      <div className="bg-gray-700">
+        <div className="py-24 bg-red-400 text-white text-center">
+          <h1 className="text-4xl font-bold mb-4">About MangaCat</h1>
           <p className="text-lg">
-            Simplify your finances, track your expenses, and achieve your
-            financial goals with our intuitive expense tracker app.
+            Keep track of completed manga, plan to read future manga, and rate
+            manga with MangaCat.
           </p>
         </div>
+        <div className="container mx-auto py-12">
+          <div className="max-w-3xl mx-auto px-4">
+            <h2 className="text-2xl text-white font-bold mb-4">Track Manga</h2>
+            <p className="text-lg text-white mb-8">
+              MangaCat allows users to track and set the current status of
+              different manga they may encounter throughout the site. For
+              example, manga can be set to the following options: Completed,
+              Planning to read, and Currently reading.
+            </p>
+            {/*
+            <h2 className="text-2xl text-white font-bold mb-4">Plan Manga</h2>
+            <p className="text-lg text-white mb-8">
+
+            </p>
+            */}
+            <h2 className="text-2xl text-white font-bold mb-4">Rate Manga</h2>
+            <p className="text-lg text-white mb-8">
+              MangaCat features the ability to allow users to rate different
+              Manga they have set to completed; allowing users to easily set
+              different scores for different mangas and help MangaCat recommend
+              new mangas to the user.
+            </p>
+            <h2 className="text-2xl text-white font-bold mb-4">
+              Manga Recommendations
+            </h2>
+            <p className="text-lg text-white mb-8">
+              MangaCat provides a recommendation feature which will recommend
+              mangas which the user currently has not set as completed based
+              upon metrics such as genres of previously completed manga, as well
+              as ratings given to those previously completed manga.
+            </p>
+          </div>
+        </div>
+        <footer className="bg-red-400 text-white py-8 text-center">
+          <div className="container mx-auto">
+            <p>MangaCat: The goto Manga Cataloging Application</p>
+          </div>
+        </footer>
       </div>
     </>
   );
