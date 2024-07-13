2ae8d7bb21f1e7b48866f717f35c6c5e5af046cb
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-12 21:30:44 -0700

    Fix options for default navigation bar

--- client/src/components/DefaultNav.js
+++ client/src/components/DefaultNav.js
@@ -2,7 +2,19 @@ import React from "react";
 
 const DefaultNav = () => {
   return (
-    <nav className="flex justify-between items-center bg-pink-300 text-black p-4 py-10"></nav>
+    <nav className="flex justify-between items-center bg-red-300 text-white p-4 py-10">
+      <div className="text-xl font-bold">
+        <a href="/">MangaCat</a>
+      </div>
+      <div>
+        <a href="/login" className="mx-8 text-xl hover:text-gray-400">
+          Login
+        </a>
+        <a href="/signup" className="mx-8 text-xl hover:text-gray-400">
+          Sign Up
+        </a>
+      </div>
+    </nav>
   );
 };
 export default DefaultNav;
