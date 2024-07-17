e62b8fc73d8339febfeb3ed2eb973545949a0b97
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-17 00:40:48 -0700

    Updated web page design of login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -62,50 +62,14 @@ const Signup = () => {
       <AboutNav />
       <div className="bg-gray-700 min-h-screen flex items-center justify-center">
         <div className=" max-w-xl text-white mx-auto p-6 mt-10">
-          <h1 className="text-4xl font-bold mb-8 text-center">Sign Up</h1>
+          <h1 className="text-4xl font-bold mb-8 text-center">Login</h1>
           <form onSubmit={handleSignUp}>
-            <div className="flex flex-col md:flex-row mb-6">
-              <div className="w-full md:w-1/2 pr-1">
-                <label
-                  className="block text-white text-sm font-bold mb-2"
-                  htmlFor="name"
-                >
-                  Username
-                </label>
-                <input
-                  className="border border-gray-300 text-black rounded-lg px-3 py-2 w-full focus:outline-none focus:border-red-300"
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Name"
-                  value={formData.name}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="w-full md:w-1/2 pl-1">
-                <label
-                  className="block text-white text-sm font-bold mb-2"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  className="border border-gray-300 text-black rounded-lg px-3 py-2 w-fill focus:outline-none focus:border-red-300"
-                  type="text"
-                  id="email"
-                  name="email"
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
             <div className="mb-6">
               <label
                 className="block text-white text-sm font-bold mb-2"
                 htmlFor="password"
               >
-                Password
+                Username or Email
               </label>
               <input
                 className="border border-gray-300 text-black rounded-lg px-3 py-2 w-full focus:outline-none focus:border-red-300"
@@ -122,7 +86,7 @@ const Signup = () => {
                 className="block text-white text-sm font-bold mb-2"
                 htmlFor="confirmpassword"
               >
-                Confirm Password
+                Password
               </label>
               <input
                 className="border border-gray-300 text-black rounded-lg px-3 py-2 w-full focus:outline-none focus:border-red-300"
