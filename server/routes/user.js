dddf7268b8ca272a629486239d7746d50dab18d9
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-18 22:01:41 -0700

    updated user login backend endpoint

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,6 +7,36 @@ const router = express.Router();
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and Password are required" });
+  }
+
+  try {
+    const query = "SELECT * FROM user WHERE email = ?";
+    connection.query(query, [email], async (error, result) => {
+      if (error) {
+        return res.status(500).json({ error: "Internal Sever Error" });
+      }
+
+      if (result.length == 0) {
+        return res
+          .status(401)
+          .json({ Login: false, error: "Invalid email or password" });
+      }
+
+      const user = result[0];
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return res
+          .status(401)
+          .json({ Login: false, error: "Invalid email or password" });
+      }
+    });
+  } catch (error) {
+    console.error("Error during login: ", error);
+    return res.status(500).json({ error: "Intenal server error" });
+  }
 });
 router.post("/signup", userRegisterValidator, register);
 
