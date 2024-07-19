676d3ba0f82b1679cb0f14b32f9b4a83515cfeac
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-18 21:42:51 -0700

    finished user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -19,3 +19,48 @@ async function createUsersTable() {
 }
 
 createUsersTable();
+
+async function register(req, res) {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide all required fields." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const insertQuery = `
+      INSERT INTO user (username, email, password) 
+      VALUES (?, ?, ?, ?)
+    `;
+
+    const values = [username, email, hashedPassword];
+
+    connection.query(insertQuery, values, (error, result) => {
+      if (error) {
+        console.error("Error during query execution: ", error);
+        res
+          .status(500)
+          .json({ message: "An error occurred while registering user." });
+      }
+
+      const user_id = result.insertId;
+
+      res.status(201).json({
+        user_id,
+        message: "User registered successfully.",
+      });
+
+      console.log("New user ID: ", user_id);
+    });
+  } catch (error) {
+    console.error("Error registering user: ", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while resgistering user." });
+  }
+}
+
+module.exports = { register };
