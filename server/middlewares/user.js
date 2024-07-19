1093b804cd28c9e4bc69f340e8298dfed9317965
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-18 21:43:23 -0700

    password validator for middleware

--- server/middlewares/user.js
+++ server/middlewares/user.js
@@ -1,7 +1,7 @@
 const { body, validationResult } = require("express-validator");
 
 const userRegisterValidator = [
-  body("name")
+  body("username")
     .notEmpty()
     .withMessage("Username is required")
     .isLength({ min: 5, max: 20 })
@@ -16,6 +16,15 @@ const userRegisterValidator = [
     .withMessage("Password is required")
     .isLength({ min: 5, max: 20 })
     .withMessage("Password must be within 5 to 20 characters"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6, max: 20 })
+    .withMessage("Password must be between 6 to 20 characters")
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/)
+    .withMessage(
+      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+    ),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
