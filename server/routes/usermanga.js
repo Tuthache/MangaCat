78889e60a9edff574bdeac083fb64550c977d449
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-07-30 18:43:48 -0700

    add rate status and remove routes added to usermanga

--- server/routes/usermanga.js
+++ server/routes/usermanga.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usermangaController = require("../controllers/usermanga.js");
 const connection = require("../config/connection.js");
 
-router.post("/add");
-router.put("/rate");
-router.delete("/remove");
+router.post("/add", usermangaController.addManga);
+router.put("/rate", usermangaController.rateManga);
+router.put("/status", usermangaController.updateStatusManga);
+router.delete("/remove", usermangaController.removeManga);
