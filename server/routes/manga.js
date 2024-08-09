e46b8d50e6f02cb8367620dd479fc67f910b52ed
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-08-08 18:25:53 -0700

    New route to get all manga in order to display onto manga list page

--- server/routes/manga.js
+++ server/routes/manga.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { retrieveManga, populateDatabase } = require("../controllers/manga.js");
+const {
+  retrieveManga,
+  populateDatabase,
+  getAllManga,
+} = require("../controllers/manga.js");
 const router = express.Router();
 
 router.get("/retrieveManga", retrieveManga);
 router.post("/populate", populateDatabase);
+router.get("/getAllManga", getAllManga);
 
 module.exports = router;
