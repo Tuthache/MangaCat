b2157bcf4af2423f5836916cd25c0efe2df5f421
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-08-10 19:14:17 -0700

    Clean up usermanga route

--- server/routes/usermanga.js
+++ server/routes/usermanga.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const usermangaController = require("../controllers/usermanga.js");
-const connection = require("../config/connection.js");
 
 router.post("/add", usermangaController.addManga);
 router.put("/rate", usermangaController.rateManga);
