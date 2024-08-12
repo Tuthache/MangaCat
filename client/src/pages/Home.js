116ab3ab5c361c2ae38a11470de19fc0d6109c67
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-08-11 22:46:38 -0700

    Side bar correctly displays username, Home calls correct endpoint

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,29 @@ import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:8000/userInfo", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Response received: ", response);
+          return response.json();
+        } else {
+          console.error("Failed to fetch user data");
+        }
+      })
+      .then((data) => {
+        console.log("Data received: ", data);
+        setUser(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching user data: ", error);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col h-screen">
       <DefaultNav />
