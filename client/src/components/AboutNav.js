bad11f70707ccc5988b301948697d909adaad724
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-09-29 12:22:42 -0700

    Changed home navigation to have picture of a Manga Cat

--- client/src/components/AboutNav.js
+++ client/src/components/AboutNav.js
@@ -4,7 +4,13 @@ const AboutNav = () => {
   return (
     <nav className="flex justify-between items-center bg-red-300 text-white p-4 py-10">
       <div className="text-xl font-bold">
-        <a href="/">MangaCat</a>
+        <a href="/">
+          <img
+            src={require("../img/Cat.PNG")}
+            alt="MangaCat"
+            className="w-30 h-20 rounded-lg"
+          ></img>
+        </a>
       </div>
       <div>
         <a href="/login" className="mx-8 text-xl hover:text-gray-400">
--- client/src/components/DefaultNav.js
+++ client/src/components/DefaultNav.js
@@ -4,7 +4,13 @@ const DefaultNav = () => {
   return (
     <nav className="flex justify-between items-center bg-red-300 text-white p-4 py-10">
       <div className="text-xl font-bold">
-        <a href="/home">MangaCat</a>
+        <a href="/home">
+          <img
+            src={require("../img/Cat.PNG")}
+            alt="MangaCat"
+            className="w-30 h-20 rounded-lg"
+          ></img>
+        </a>
       </div>
       <div>
         <a href="/recommendation" className="mx-8 text-xl hover:text-gray-400">
